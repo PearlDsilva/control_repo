1d5a62d1ba0dbd37f8fb35de51ba0ef799f2980b
Pearl1594 <pearl1594@gmail.com>
2019-03-15 14:07:50 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,22 +1,6 @@
 node default{
 
-	#Apt::Source <| |> -> Class['apt::update'] -> Package <| |>
-	#include ::docker	
-	#include 'docker'
-	#docker::image { 'ubuntu':
-  	#	image_tag => 'trusty'
-	#}	
-#	include role::master_server
-#	file { '/root/README':
-#		ensure => file,
-#		content => 'this is a README',
-#		owner   => root,
-		
-#	}
-	# Install package - vim on the node
-#	package { 'vim':
-#		ensure => present,
-#	}	
+
 }
 
 node ubuntu {
