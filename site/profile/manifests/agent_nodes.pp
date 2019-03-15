60684a16255134e6b2a0ab2be1bd3d770ea646ac
Pearl1594 <pearl1594@gmail.com>
2019-03-15 13:43:22 +0530

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,8 +1,12 @@
 class profile::agent_nodes{
   include docker
   include dockeragent
-  dockeragent::node {'web.puppet.vm':}
-  #dockeragent::node {'db.puppet.vm':}
+  # dockeragent::node {'web.puppet.vm':}
+  # dockeragent::node {'db.puppet.vm':}
+  docker::run { 'web.puppet.vm':
+  image   => 'ubuntu:latest',
+  command => '/bin/sh -c "while true; do echo hello world; sleep 1; done"',
+  }
   docker::run { 'db.puppet.vm':
   image   => 'ubuntu:latest',
   command => '/bin/sh -c "while true; do echo hello world; sleep 1; done"',
