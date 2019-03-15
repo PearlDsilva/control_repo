ee3cee45d5a4a255e1f7d6fa9455a18a9d0220a7
Pearl1594 <pearl1594@gmail.com>
2019-03-15 11:55:44 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,18 @@
 node default{
-	
+
+ 	apt::key { '9DC858229FC7DD38854AE2D88D81803C0EBFCD88':
+    		source => 'https://download.docker.com/linux/ubuntu/gpg',
+	  } ->
+  	apt::source { 'docker-ce':
+    		architecture => 'amd64',
+  		location     => 'https://download.docker.com/linux/ubuntu',
+   		repos        => 'stable',
+   		release      => $::lsbdistcodename,
+ 	 } ->
+ 	 package { 'docker-ce':
+  		ensure  => 'latest',
+    		require => Exec['apt_update'],
+  	}
 
 	include ::docker 
 	file { '/root/README':
