abe46339d8e27bdd4018b1321cf43330329d3931
Pearl1594 <pearl1594@gmail.com>
2019-03-15 14:42:29 +0530

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -7,7 +7,7 @@ class profile::agent_nodes{
   #image   => 'ubuntu:latest',
   image => 'agent',
   net => 'dockeragent-net'
-  command          => '/usr/lib/systemd/systemd',
+  command          => '/lib/systemd/systemd',
   env              =>  [
    'RUNLEVEL=3',
    'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/bin',
@@ -19,7 +19,7 @@ class profile::agent_nodes{
   docker::run { 'db.puppet.vm':
   image => 'agent',
   net => 'dockeragent-net'
-  command          => '/usr/lib/systemd/systemd',
+  command          => '/lib/systemd/systemd',
    env              =>  [
     'RUNLEVEL=3',
     'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/bin',
