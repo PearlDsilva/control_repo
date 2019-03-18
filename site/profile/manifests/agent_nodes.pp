43934390216caefd1c7e617faf8bd2fca12fdb9c
Pearl1594 <pearl1594@gmail.com>
2019-03-18 12:07:15 +0530

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,32 +1,7 @@
 class profile::agent_nodes{
   include docker
-  #include dockeragent
-  #dockeragent::node {'web.puppet.vm':}
-  #dockeragent::node {'db.puppet.vm':}
-  docker::run { 'web.puppet.vm':
-  #image   => 'ubuntu:latest',
-  image => 'puppet/puppet-agent-ubuntu',
-  net => 'dockeragent-net',
-  command => 'puppet agent --server=ubuntu -t',
-  env =>  [
-   'RUNLEVEL=3',
-   'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/bin',
-   'HOME=/root/',
-   'TERM=xterm'
-  ],
-
-  }
-  docker::run { 'db.puppet.vm':
-  image => 'puppet/puppet-agent-ubuntu',
-  net => 'dockeragent-net',
-  #command => '/usr/lib/systemd/systemd',
-  env =>  [
-    'RUNLEVEL=3',
-    'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/bin',
-    'HOME=/root/',
-    'TERM=xterm'
-   ],
-
-   }
+  include dockeragentnew
+  dockeragentnew::node {'web.puppet.vm':}
+  dockeragentnew::node {'db.puppet.vm':}
 }
   
