19591c0fc0b4750722f99e9d0ecd918c09a402b2
Pearl1594 <pearl1594@gmail.com>
2019-03-18 14:17:42 +0530

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -19,7 +19,13 @@ class minecraft (
   }
   file { '/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
+    # source => 'puppet:///modules/minecraft/minecraft.service',
+    # This is when we have created a template for dynamically loading the service file so, we use function here the funtion is epp itself
+    # as we created a minecraft.service.epp template file
+    # syntax : function('module_name/directory_name/(or)file_name',{parameters_to_be_passed_to template})
+    content => epp('minecraft/minecraft.service',{
+        install_dir => $install_dir,
+      })
   }
   service { 'minecraft':
     ensure => running,
