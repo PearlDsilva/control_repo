af101ef0947a8f00434563509dedaa64f3a9a0c8
Pearl1594 <pearl1594@gmail.com>
2019-04-10 10:55:45 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,7 @@
 node ubuntu {
-  hiera_include(classes-debian)
-  }
+  #hiera_include(classes-debian)
+  $packages = loopup(classes-debian)
+  
+  package { $packages:
+    ensure => installed,
+    }
